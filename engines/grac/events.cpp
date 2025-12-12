3e49063b5ad03e9f6d0b40e041fa99e3b7de2574
denzquix <denzquix@gmail.com>
2025-12-12 19:19:33 +0000

    Fix bounds to new resolution

--- engines/grac/events.cpp
+++ engines/grac/events.cpp
@@ -193,7 +193,7 @@ void Events::addKeypress(const Common::KeyCode kc) {
 /*------------------------------------------------------------------------*/
 
 Bounds::Bounds(Common::Rect &innerBounds) :
-		_bounds(0, 0, 320, 200),
+		_bounds(0, 0, 640, 512),
 		_innerBounds(innerBounds),
 		left(_bounds.left), top(_bounds.top),
 		right(_bounds.right), bottom(_bounds.bottom) {
